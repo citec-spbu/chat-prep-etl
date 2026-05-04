49f5317b7de35364818a80c22d21468b5361cfd5
1baediv <edikdbaimetovs@gmail.com>
2026-05-04 12:22:34 +0300

    init script? downloaded zip

---
+++ src/etl/adapter/yd_parser.py
@@ -0,0 +1,4 @@
+from typing import Any, List
+from bs4 import BeautifulSoup
+from src.etl.internal.domain.interfaces import IParser
+from src.etl.internal.domain.value_objects import MessageMetadata
