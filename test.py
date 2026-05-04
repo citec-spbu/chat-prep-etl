7592839a35ae8e2b599280273965a22901c3b071
1baediv <edikdbaimetovs@gmail.com>
2026-05-04 20:17:19 +0300

    реализованы возможонсти парсинга html чатов и скачивание с яд архива с чатами, подстроенный под юзкейс коллеги алины

--- test.py
+++ test.py
@@ -1,37 +1,42 @@
-import json
-from telethon import TelegramClient
-import socks
 import asyncio
-from dotenv import load_dotenv
+from src.etl.adapter.yd_parser import HTMLParser
 import os
-from src.etl.internal.adapter.tg_grabber import TelegramParser, TelegramGrabber
 
-# 1. Настройки (подставь свои или возьми из .env)
-load_dotenv()
-api_id = int(os.getenv('TELEGRAM_API_ID'))  
-api_hash = os.getenv('TELEGRAM_API_HASH')  
-session_name = os.getenv('SESSION')
-chat_username =  "https://t.me/..."
 
-proxy = (socks.SOCKS5, '127.0.0.1', 10808)        
+async def test_parser():
+    # Твой HTML контент (вставь сюда фрагмент или читай из файла)
+    html_content = "zipdata/ChatExport_2026-04-28/messages.html"  # Путь к тестовому HTML файлу
+    if not os.path.exists(html_content):
+            # Если файла нет, используем строку для теста
+            print("Файл не найден, используем захардкоженную строку...")
+            html_content = """
+            <div class="message default clearfix">
+                <div class="from_name">Константин</div>
+                <div class="text">Привет, это тест 1</div>
+            </div>
+            <div class="message default clearfix">
+                <div class="from_name">Dzen</div>
+                <div class="text">Привет, это тест 2</div>
+            </div>
+            """
+    else:
+        # ЧИТАЕМ ФАЙЛ
+        with open(html_content, "r", encoding="utf-8") as f:
+            html_content = f.read()
 
-async def debug_run():
-    client = TelegramClient(session_name, api_id, api_hash, proxy=proxy)
-    parser = TelegramParser(client)
-    grabber = TelegramGrabber(client, parser)
-
-    messages = await grabber.grab_chat(chat_username, limit=10)
+    parser = HTMLParser()
     
-    print(f"--- Результаты парсинга ({len(messages)} шт.) ---")
+    # 2. ПРОВЕРКА: Что реально уходит в парсер
+    print(f"Длина контента для парсинга: {len(html_content)} символов")
     
-    for metadata in messages:
-        print("\n" + "="*30)
-        print(f"Parsed Chat ID: {metadata.chat_id}")
-        print(f"Текст: {metadata.text[:50]}...") 
-        print(f"Пути к файлам: {metadata.attached_files}")
-        print("="*30)
+    messages = await parser.parse_batch(html_content)
 
-    await client.disconnect()
+    print(f"Найдено сообщений: {len(messages)}")
+    
+    for m in messages:
+        print(f"Отправитель: {m.sender_id} | Текст: {m.text[:30]}")
+
+    assert len(messages) >= 2, f"Ожидали >= 2, получили {len(messages)}"
 
 if __name__ == "__main__":
-    asyncio.run(debug_run())
+    asyncio.run(test_parser())
