b576c0ec4732ac14abb770ea86b4d618578a6e40
ne-F1anders <sosnovgreg@gmail.com>
2026-05-06 22:28:14 +0000

    feat: main is updated for config-processing, logging, initializing orchestrator. results of the experiment are saved near the experiments, but gitignored

--- src/testing_system/cmd/main.py
+++ src/testing_system/cmd/main.py
@@ -1,8 +1,35 @@
-from testing_system.cmd.test_mock_pipeline import mock_test
-from testing_system.cmd.test_ollama_pipeline import ollama_test
+import asyncio
+import sys
+import logging
+import logging.config
+import yaml
+#from aiohttp import web
+from testing_system.internal.adapter.assistants.ollama import OllamaAssistant
+from testing_system.internal.adapter.assistants.mock import MockAssistant
+from testing_system.internal.adapter.retrievers.mock import MockRetriever
+from testing_system.internal.controller.Orchestrator import Orchestrator
+from testing_system.internal.domain.value_objects import RetrievedDocument
 
-mock_test()
-print("\n")
-ollama_test()
+async def main():
+    try:
+        config_path = sys.argv[1] #Usage: python -m testing_system.cmd.main <config.yaml>
+    except:
+        config_path = "testing_system/config/config.yaml"
+    with open(config_path, 'r') as f:
+        cfg = yaml.safe_load(f)
 
-# logger is used in Runner, be careful - init the logger not to get error
+    logging.config.dictConfig(cfg.get("logging", {}))
+    logger = logging.getLogger(__name__)
+    logger.info("Starting Testing System")
+    orchestrator = Orchestrator(cfg["testing_system"]) #TODO
+    await orchestrator.execute_experiments(cfg["testing_system"]["loaders"]["path"])
+    '''
+    server_port = cfg.get("testing_system", {}).get("port", 2222)
+    logger.info("HTTP server listening on port %d", server_port)
+    web.run_app(app, port=server_port)
+    '''
+    print("Done")
+    print(orchestrator.runner.registry.select(None))
+
+if __name__ == "__main__":
+    asyncio.run(main())
