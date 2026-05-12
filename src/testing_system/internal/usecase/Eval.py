262aa453993db4c983483ae81afc9b9e79256900
ne-F1anders <sosnovgreg@gmail.com>
2026-05-12 09:19:56 +0000

    feat: Experiment add method is updated for experiment setting (trial), experiment_2 initialized with gemma and 3 new metrics (detail in notebook)

--- src/testing_system/internal/usecase/Eval.py
+++ src/testing_system/internal/usecase/Eval.py
@@ -1,4 +1,7 @@
+import re
 from typing import List
+
+import numpy as np
 from testing_system.internal.domain.entities import MetricValue, Question, Answer, MetricType
 import torch
 from sentence_transformers import SentenceTransformer
@@ -13,8 +16,10 @@ class Eval:
     - bert_score
     - exact_match
     - rouge_l_f1
-    - hallucination_rate (notImplemented)
-    and the model for debug version
+    - hallucination_rate
+    - numeric_accuracy
+    - overlap
+    and the encoder-model parameter for debug version
 
     The evaluation method 'execute'
     input: Question, Answer
@@ -44,7 +49,15 @@ class Eval:
                 case "rouge_l_f1":
                     self.metric_evaluators.append(self._rouge_l_f1)
                 case "hallucination_rate":
+                    if not self.encoder:
+                        self.encoder = SentenceTransformer(
+                            model
+                        )
                     self.metric_evaluators.append(self._hallucination_rate)
+                case "numeric_accuracy":
+                    self.metric_evaluators.append(self._numeric_accuracy)
+                case "overlap":
+                    self.metric_evaluators.append(self._overlap)
 
     def _latency(self, q: Question, a: Answer) -> MetricValue:
         return MetricValue(
@@ -84,13 +97,61 @@ class Eval:
                 }
         )
     def _hallucination_rate(self, q: Question, a: Answer) -> MetricValue:
-        '''pred_emb = self.encoder.encode(a.text)
-        doc_emb = self._embed(source_doc)
-        # Косинусное сходство (векторы нормализованы, поэтому просто скалярное произведение)
-        sim = float(np.dot(pred_emb, doc_emb))
-        return 1.0 - sim'''
-        raise NotImplementedError
-
+        sentences = re.split(r'[.!?]+', '\n'.join(i.content for i in a.retrieved_context))
+        sentences = [s.strip() for s in sentences if s.strip()]
+        if not sentences:
+            return 0.0
+        pred_emb = self.encoder.encode([a.text])[0]
+        sentences_embs = self.encoder.encode(sentences)
+        similarities = self.encoder.similarity(pred_emb, sentences_embs)[0]
+        return MetricValue(
+            type = MetricType.HALLUCINATION_RATE,
+            value = float(np.max(similarities))
+        )
+    def _numeric_accuracy(self, q: Question, a: Answer) -> MetricValue:
+        pred_numbers = set(re.findall(r'-?\d+/\d+|-?\d+\.\d+|-?\d+', a.text))
+        ref_numbers = set(re.findall(r'-?\d+/\d+|-?\d+\.\d+|-?\d+', q.ground_true))
+        if not ref_numbers:  
+            return 1.0 
+        if not pred_numbers:
+            return 0.0
+        intersection = pred_numbers.intersection(ref_numbers)
+        return MetricValue(
+            type = MetricType.NUMERIC_ACCURACY,
+            value = len(intersection) / len(ref_numbers)
+        )
+    def _overlap(self, q: Question, a: Answer) -> MetricValue:
+        """
+        3 overlap metrics:
+        - overlap_recall: the proportion of ethalon tokens in answer
+        - overlap_precision: the proportion of given tokens to ethalon's
+        - overlap_symmetric: the intersection divided by minimum size (as a metric default value)
+        """
+        pred_tokens = set(re.findall(r'\w+', a.text.lower()))
+        ref_tokens = set(re.findall(r'\w+', q.ground_true.lower()))
+        
+        intersection = pred_tokens.intersection(ref_tokens)
+        inter_size = len(intersection)
+        
+        recall = inter_size / len(ref_tokens) if ref_tokens else (1.0 if not pred_tokens else 0.0)
+        precision = inter_size / len(pred_tokens) if pred_tokens else (1.0 if not ref_tokens else 0.0)
+        
+        if not pred_tokens and not ref_tokens:
+            symmetric = 1.0
+        elif not pred_tokens or not ref_tokens:
+            symmetric = 0.0
+        else:
+            symmetric = inter_size / min(len(pred_tokens), len(ref_tokens))
+        
+        return MetricValue(
+            type = MetricType.OVERLAP,
+            value = symmetric,
+            metadata={
+                "overlap_recall": recall,
+                "overlap_precision": precision,
+            }
+        )
+    
     def execute(self, q: Question, a: Answer) -> List[MetricValue]:
         metrics = []
 
